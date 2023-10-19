0d6302556308ee101c4df30f09f771635489ddd9
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-19 20:50:11 +0100

    initial commit

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Write a script that reads stdin line by line and computes metrics:
+
+Input format:
+
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size> 
+
+(if the format is not this one, the line must be skipped)
+
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning:
+    - Total file size: File size: <total size>
+    - where <total size> is the sum of all previous <file size> 
+      (see input format above)
+
+Number of lines by status code:
+    - possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+    - if a status code doesn’t appear or is not an integer,
+      don’t print anything for this status code
+    - format: <status code>: <number>
+status codes should be printed in ascending order
+"""
+import sys
+
+
+if __name__ == '__main__':
+    badge = 0
+    total_size = 0
+    code_dict = {200: 0, 301: 0, 400: 0, 401: 0,
+                 403: 0, 404: 0, 405: 0, 500: 0}
+    for line in sys.stdin:
+        words = line.rstrip().split()
+        if (len(words) != 9) or ('"GET' not in words) \
+                or ('HTTP/1.1"' not in words):
+            continue
+        code = int(words[-2])
+
+        try:
+            code_dict[code] += 1
+        except:
+            pass
+
+        total_size += int(words[-1])
+        badge += 1
+
+        try:
+            # Print stats after 10 badges
+            if not badge % 10:
+                print("File size: {}".format(total_size))
+                total_size = 0
+                for k, v in code_dict.items():
+                    print("{}: {}".format(k, v))
+                    code_dict[k] = 0
+        except KeyboardInterrupt:
+            # Print stats after keyboard interrupt, not working yet
+            print("File size: {}".format(total_size))
+            total_size = 0
+            for k, v in code_dict.items():
+                print("{}: {}".format(k, v))
+                code_dict[k] = 0
+            raise KeyboardInterrupt
