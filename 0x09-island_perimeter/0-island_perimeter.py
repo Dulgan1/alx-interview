e3b57a518ad7b239eeacc221d27343627a9c323d
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-12-01 02:19:12 +0100

    Chnaged to  handle out of range

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,18 +8,37 @@
  Use with  test file 5-main.py, default grid perimeter is 12,
  tweak grid for different perimeter.
 """
+# v1.0
+#def island_perimeter(space):
+#    """ Function to get perimeter"""
+#    pmetre = 0
+#    for x in range(len(space)):
+#        for y in range(len(space[x])):
+#            if space[x][y] == 1:
+#                if space[x - 1][y] == 0: # check cell above
+#                    pmetre += 1
+#                if space[x + 1][y] == 0: # check cell below
+#                    pmetre += 1
+#                if space[x][y - 1] == 0: # check cell by left
+#                    pmetre += 1
+#                if space[x][y + 1] == 0: # check cell by right
+#                    pmetre += 1
+#    return pmetre
+
 def island_perimeter(space):
-    """ Function to get perimeter"""
+    """ Function to get parametre """
     pmetre = 0
     for x in range(len(space)):
-        for y in range(len(space[x])):
+        for y in range(len(space[0])):
             if space[x][y] == 1:
-                if space[x - 1][y] == 0: # check cell above
+                if x == 0 or space[x - 1][y] == 0:
                     pmetre += 1
-                if space[x + 1][y] == 0: # check cell below
+                if x == (len(space) - 1) or \
+                        space[x + 1][y] == 0:
                     pmetre += 1
-                if space[x][y - 1] == 0: # check cell by left
+                if y == 0 or space[x][y - 1] == 0:
                     pmetre += 1
-                if space[x][y + 1] == 0: # check cell by right
+                if y == (len(space[0]) - 1) or \
+                        space[x][y + 1] == 0:
                     pmetre += 1
     return pmetre
