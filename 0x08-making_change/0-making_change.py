0c00d13e9b4f2ee98bf1a2b641aeac1733c954b5
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-12-02 00:50:17 +0100

    PEP8

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,11 +7,11 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
     coins.sort(reverse=True)
-    n_coins = 0 # number of coins to complete change (total)
-    
+    n_coins = 0  # number of coins to complete change (total)
+
     for coin in coins:
         if total % coin < total:
             n_coins += total // coin
             total = total % coin
-    
+
     return n_coins if total == 0 else -1
