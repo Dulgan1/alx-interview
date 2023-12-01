dc718c965dc89a5d8761e2f4545df0c853676bcf
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-12-02 00:46:31 +0100

    Initial commit

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Making change problem"""
+
+
+def makeChange(coins, total):
+    """making change as total"""
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    n_coins = 0 # number of coins to complete change (total)
+    
+    for coin in coins:
+        if total % coin < total:
+            n_coins += total // coin
+            total = total % coin
+    
+    return n_coins if total == 0 else -1
